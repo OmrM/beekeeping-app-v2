9b0d33497dd23f0945daf8b655f62dc1719bf84b
OmrM <omargmuniz@gmail.com>
2023-05-25 05:29:47 -0600

    renamed SettingStackNavigator

--- navigation/HomeStackNavigator.tsx
+++ navigation/HomeStackNavigator.tsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
-import SettingsScreen from "../screens/SettingScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 import { HeaderBackButtonProps } from "@react-navigation/native-stack/lib/typescript/src/types";
 const Stack = createNativeStackNavigator();
 
