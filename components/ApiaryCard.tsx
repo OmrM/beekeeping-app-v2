016289b6a4e201fd39bc7a389f628c0ba1bae26f
OmrM <omargmuniz@gmail.com>
2023-06-03 04:58:04 -0600

    added cardtextcontainer

--- components/ApiaryCard.tsx
+++ components/ApiaryCard.tsx
@@ -2,14 +2,14 @@
 */
 import React from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { CardContainer, CardTitle, CardText } from './styles/CardContainer.styles';
+import { CardContainer, CardTitle, CardText, CardTextContainer } from './styles/CardContainer.styles';
 
 // Apiary Interface: also used in the ApiariesScreen.tsx
 export interface Apiary {
     name: string;
     hives: number;
     location: string;
-    notes:  string;
+    notes: string;
 }
 
 interface ApiaryCardProps {
@@ -17,14 +17,16 @@ interface ApiaryCardProps {
     onPress: () => void;
 }
 
-const ApiaryCard = ({item, onPress}: ApiaryCardProps) => {
+const ApiaryCard = ({ item, onPress }: ApiaryCardProps) => {
     return (
         <TouchableOpacity onPress={onPress}>
             <CardContainer>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>Hives: {item.hives}</CardText>
-                <CardText>Location: {item.location}</CardText>
-                <CardText>Notes: {item.notes}</CardText>
+                <CardTextContainer>
+                    <CardTitle>{item.name}</CardTitle>
+                    <CardText>Hives: {item.hives}</CardText>
+                    <CardText>Location: {item.location}</CardText>
+                    <CardText>Notes: {item.notes}</CardText>
+                </CardTextContainer>
             </CardContainer>
         </TouchableOpacity>
     );
