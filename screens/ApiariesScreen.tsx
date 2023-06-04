1837c44e59af04bf48aab3961cecab94deb6a03d
OmrM <omargmuniz@gmail.com>
2023-06-04 11:14:11 -0600

    succesfully ran amplify push

--- screens/ApiariesScreen.tsx
+++ screens/ApiariesScreen.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, } from 'react-native';
 import { Container } from './styles/Screens.styles';
 import ActionButton from '../components/ActionButton';
 
 import ApiaryCard from '../components/ApiaryCard';
 import { Apiary } from '../components/ApiaryCard';
-
+import { API } from 'aws-amplify'
+import {listApiaries} from '../amplify/backend/api/beekeepingappv2/'
 // Let's simulate a data from the server
 const apiaries = [
   { id: '1', name: 'Apiary 1', hives: 5, location: 'Farmville, VA', notes: 'Beside the apple orchard.' },
@@ -14,6 +15,9 @@ const apiaries = [
 ];
 
 const ApiariesScreen = () => {
+  useEffect(() => {
+
+  });
   /* custom render function for the ApiaryCard component 
     { item: Apiary } -- destructuring the item property
     and specifying its type as the 'Apiary' interface
