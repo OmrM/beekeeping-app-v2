2a33ff41aa19c0b38d1296fd19007d38a6b190c3
OmrM <omargmuniz@gmail.com>
2023-06-06 15:20:40 -0600

    added getApiaries functionality

--- screens/ApiariesScreen.tsx
+++ screens/ApiariesScreen.tsx
@@ -1,44 +1,82 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, } from 'react-native';
 import { Container } from './styles/Screens.styles';
 import ActionButton from '../components/ActionButton';
 
 import ApiaryCard from '../components/ApiaryCard';
 import { Apiary } from '../components/ApiaryCard';
-import { API } from 'aws-amplify'
-import {listApiaries} from '../amplify/backend/api/beekeepingappv2/'
-// Let's simulate a data from the server
-const apiaries = [
-  { id: '1', name: 'Apiary 1', hives: 5, location: 'Farmville, VA', notes: 'Beside the apple orchard.' },
-  { id: '2', name: 'Apiary 2', hives: 3, location: 'Lynchburg, VA', notes: 'Next to the creek.' },
-  //... more apiaries
-];
+import { API, graphqlOperation } from 'aws-amplify'
+import { GraphQLQuery } from '@aws-amplify/api';
+import { CreateApiaryInput, CreateApiaryMutation, GetApiaryQuery, ListApiariesQuery,  } from '../src/API';
+import { createApiary } from '../src/graphql/mutations';
+import { listApiaries } from '../src/graphql/queries';
+
 
 const ApiariesScreen = () => {
-  useEffect(() => {
+  const [apiaries, setApiaries] = useState<Apiary[]>([]); // setting it as the type Apiary from our card component
 
-  });
+
+
+
+
+  useEffect(() => {
+    getApiaries();
+
+  }, []);
+
+
+/*   const apiaryData = [
+    { id: '1', name: 'Apiary 1', hives: 5, location: 'Farmville, VA', notes: 'Beside the apple orchard.' },
+    { id: '2', name: 'Apiary 2', hives: 3, location: 'Lynchburg, VA', notes: 'Next to the creek.' },
+    //... more apiaries
+  ]; */
+
+
+
+/*   const getApiaries = async () => {
+    const apiaryData = await API.graphql<GraphQLQuery<ListApiariesQuery>>({
+      query: listApiaries
+    });
+    if (!apiaryData.data || !apiaryData.data.listApiaries) {
+      console.log('No apiaries found');
+      return;
+    }
+    const apiaryDataItems = apiaryData.data.listApiaries.items ?? [];
+    setApiaries(apiaryDataItems as unknown as Apiary[]);
+  };
+ */
+
+  const getApiaries = async () => {
+    try{
+      const apiaryData = await API.graphql<GraphQLQuery<ListApiariesQuery>>({ query: listApiaries });
+      const apiaryDataItems = apiaryData.data?.listApiaries?.items ?? [];
+      console.log(JSON.stringify(apiaryDataItems))
+      setApiaries(apiaryDataItems as unknown as Apiary[]);
+    } catch (error) {
+      console.error('Error fetching apiaries', error);
+    }
+  };
+
+  const handleActionBttnPress = async () => {
+    console.log("pressed");
+
+  };
+
+
   /* custom render function for the ApiaryCard component 
-    { item: Apiary } -- destructuring the item property
-    and specifying its type as the 'Apiary' interface
-    this allows direct access to item and enforces type checking
+      { item: Apiary } -- destructuring the item property
+      and specifying its type as the 'Apiary' interface
+      this allows direct access to item and enforces type checking
   */
   const renderApiaryCard = ({ item }: { item: Apiary }) => {
     const handleApiaryCardPress = () => {
       console.log("card pressed", item.name);
     }
-
     return (
       /* ApiaryCard takes in a prop onPress, takes in handleApiaryCardPress from this render function */
       <ApiaryCard item={item} onPress={handleApiaryCardPress} />
     );
   };
-
-  const handleActionBttnPress = () => {
-    console.log("pressed");
-  };
-
-
   return (
     <Container>
       <FlatList
@@ -46,7 +84,7 @@ const ApiariesScreen = () => {
         renderItem={renderApiaryCard}
         keyExtractor={item => item.id}
       />
-      <ActionButton onPress={() => console.log("Pressed")}/>
+      <ActionButton onPress={() => console.log("Pressed")} />
     </Container>
   );
 };
