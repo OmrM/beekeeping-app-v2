400288d3aee8dd5ef319ca8a7ca2e375b45e226e
OmrM <omargmuniz@gmail.com>
2023-06-10 16:25:58 -0600

    added drop shadow to action button

--- components/styles/ActionButton.styles.ts
+++ components/styles/ActionButton.styles.ts
@@ -1,5 +1,6 @@
 /* ActionButtons.styles.ts */
 
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 
 export const ButtonWrapper = styled.View`
@@ -15,9 +16,20 @@ export const CircleButton = styled.TouchableOpacity`
     justify-content: center;
     align-items: center;
     border-radius: 30px;
+    elevation: 3; 
+    ${Platform.OS === 'ios' && `
+        shadow-color: black;
+        shadow-radius: 10px;
+        shadow-opacity: 0.2;
+    `}
 `;
 
 export const ButtonText = styled.Text`
     color: ${ ({theme}) => theme.button.textColor};
     font-size: 24px;
 `;
+
+/* shadowColor: 'black',
+shadowRadius: 10,
+shadowOpacity: 0.2,
+shadowOffset: {width: 0 , height: 4}, */
