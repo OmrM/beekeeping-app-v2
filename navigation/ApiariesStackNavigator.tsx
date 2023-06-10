3892d01bc0e84af24502f978f71f21f7d53ed2cc
OmrM <omargmuniz@gmail.com>
2023-06-10 16:24:33 -0600

    added newApiariesScreen

--- navigation/ApiariesStackNavigator.tsx
+++ navigation/ApiariesStackNavigator.tsx
@@ -3,12 +3,12 @@ import ApiariesScreen from "../screens/ApiariesScreen";
 import { ThemeContext } from "styled-components";
 import { lightTheme } from "../themes/themes";
 import { useContext } from "react";
+import AddNewApiary from "../screens/NewApiaryScreen";
+import NewApiaryScreen from "../screens/NewApiaryScreen";
 
 const Stack = createNativeStackNavigator();
 
-interface Props {
-    toggleTheme: () => void;
-}
+
 
 //const ApiariesStackNavigator: React.FC<Props> = ({toggleTheme}) => {
 const ApiariesStackNavigator = ({ toggleTheme }: Props) => {
@@ -26,6 +26,14 @@ const ApiariesStackNavigator = ({ toggleTheme }: Props) => {
             {/* <Stack.Screen name="Apiaries" component={ApiariesScreen}/> */}
             <Stack.Screen name="Apiaries">
                 {props => <ApiariesScreen {...props} toggleTheme={toggleTheme} />}
+            </Stack.Screen>
+            <Stack.Screen name="New Apiary"
+                options={{
+                    presentation: 'modal',
+                    headerBackTitle: 'cancel',
+                }}
+            >
+                {props => <NewApiaryScreen{...props} toggleTheme={toggleTheme}/>}
             </Stack.Screen>
         </Stack.Navigator>
     );
